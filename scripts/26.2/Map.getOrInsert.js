692ee4f9bb37856917c2ea05a54d2e85cd8495f7
iCrazeiOS <39101269+iCrazeiOS@users.noreply.github.com>
2026-06-26 21:03:14 +0100

    Add Map.getOrInsert/getOrInsertComputed polyfills

---
+++ scripts/26.2/Map.getOrInsert.js
@@ -0,0 +1,27 @@
+if (typeof Map !== 'undefined') {
+    function defineMapMethod(name, fn) {
+        if (!Map.prototype[name]) {
+            Object.defineProperty(Map.prototype, name, {
+                value: fn,
+                writable: true,
+                configurable: true
+            });
+        }
+    }
+
+    defineMapMethod('getOrInsert', function (key, value) {
+        if (this.has(key)) return this.get(key);
+
+        this.set(key, value);
+        return value;
+    });
+
+    defineMapMethod('getOrInsertComputed', function (key, callback) {
+        if (this.has(key)) return this.get(key);
+        if (typeof callback !== 'function') throw new TypeError('Expected a function');
+
+        const value = callback(key);
+        this.set(key, value);
+        return value;
+    });
+}
